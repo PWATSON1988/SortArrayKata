b49dbf746e52caa1144fe7357f585b3377ae2800
Watson <Paul.Watson@tui.co.uk>
2019-03-01 16:24:57 +0000

    Small tweak to description.

--- KataService/SortArray.cs
+++ KataService/SortArray.cs
@@ -5,7 +5,7 @@ namespace Kata.SortArray
     public class SortArrayKata
     {
 
-        //Your task is to write a function named “SortArray” that accepts a list of numbers. This function must return the list back but sorted in a way so that odd numbers within are sorted in ascending order with the even numbers remaining in the same position. Zero isn't an odd number and as such doesn’t need to move. If you have an empty array, this needs to be returned as such.
+        //Your task is to write a function named “SortArray” that accepts an array of numbers. This function must return the array back but sorted in a way so that odd numbers are sorted in ascending order with the even numbers remaining in the same position. Zero isn't an odd number and as such doesn’t need to move. If you have an empty array, this needs to be returned as such.
 
         //Example
         //Input = [5, 3, 2, 8, 1, 4] 
